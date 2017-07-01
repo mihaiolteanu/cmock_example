15490dd770e659dfc1169cfe8297af1c90cd8566
mihaiolteanu <mihai_olteanu@fastmail.fm>
2017-07-01 18:59:49 +0300

    Makefile unity working version

---
+++ .dir-locals.el
@@ -0,0 +1,12 @@
+;; Assume a makefile is present at project root and that dir-locals
+;; (this file) is also at project root. Find the project root through
+;; dir-locals and set the compile command. The project can now be
+;; compiled from any project buffer.
+((nil . ((eval . (set (make-local-variable 'compile-command)
+		      (concat "make -f "
+			      (file-name-directory
+			       (let ((d (dir-locals-find-file ".")))
+				 (if (stringp d) d (car d))))
+			      "makefile"))))))
+
+
---
+++ test/test_add.c
@@ -0,0 +1,17 @@
+#include "add.h"
+#include "unity.h"
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+void test_add_positive_numbers() {
+    TEST_ASSERT_EQUAL(10, add(4, 6));
+    TEST_ASSERT_EQUAL(20, add(14, 6));
+}
+
+void test_add_to_zero() {
+    TEST_ASSERT_EQUAL(5, add(0, 5));
+}
