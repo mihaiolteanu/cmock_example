65d60829e7406a45c219ffeec738db71b1a107b5
mihaiolteanu <mihai_olteanu@fastmail.fm>
2017-07-02 21:58:23 +0300

    Make out-of-source builds from anywhere

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,11 +2,13 @@
 ;; (this file) is also at project root. Find the project root through
 ;; dir-locals and set the compile command. The project can now be
 ;; compiled from any project buffer.
-((nil . ((eval . (set (make-local-variable 'compile-command)
-		      (concat "make -f "
-			      (file-name-directory
-			       (let ((d (dir-locals-find-file ".")))
-				 (if (stringp d) d (car d))))
-			      "makefile"))))))
 
-
+((nil . ((eval . (let ((root-path (file-name-directory
+				   (let ((d (dir-locals-find-file ".")))
+				     (if (stringp d) d (car d))))))
+		   (set (make-local-variable 'compile-command)
+			;; cmake -H. -Bbuild && make -C build/ >/dev/null && ./run_tests
+			(concat "cmake -H" root-path
+				" -B" root-path "build"
+				" && make -C " root-path "build/ >/dev/null"
+				" && " root-path "run_tests")))))))
